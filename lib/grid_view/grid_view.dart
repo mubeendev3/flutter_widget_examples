055937f9cbae20f42a10811a4c3cb22a9be9e048
Mubeen Mehmood <mubeendev3@gmail.com>
2023-09-19 22:15:28 +0500

    How GridView.extent works?

--- lib/grid_view/grid_view.dart
+++ lib/grid_view/grid_view.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 class CustomGridView extends StatelessWidget {
   const CustomGridView({Key? key}) : super(key: key);
-  //Use GridView.count() when you know the number of childrens
+
+  //Use GridView.count() when you know the number of children
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,37 +11,63 @@ class CustomGridView extends StatelessWidget {
         centerTitle: true,
         title: const Text("Practicing GridView"),
       ),
-      body: GridView.count(
+      body: GridView.extent(
         padding: const EdgeInsets.all(8),
-        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-        mainAxisSpacing: 10,
         crossAxisSpacing: 10,
-        crossAxisCount: 2,
+        mainAxisSpacing: 10,
+        maxCrossAxisExtent: 100,
         children: [
-          Container(
-            color: Colors.pink,
-          ),
-          Container(
-            color: Colors.red,
-          ),
-          Container(
-            color: Colors.green,
-          ),
-          Container(
-            color: Colors.orange,
-          ),
-          Container(
-            color: Colors.purple,
-          ),
-          Container(
-            color: Colors.yellow,
-          ),
-          Container(
-            color: Colors.cyan,
-          ),
+          _buildGridViewItem(0),
+          _buildGridViewItem(1),
+          _buildGridViewItem(2),
+          _buildGridViewItem(3),
+          _buildGridViewItem(4),
         ],
       ),
+      // body: GridView.count(
+      //   padding: const EdgeInsets.all(8),
+      //   keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+      //   mainAxisSpacing: 10,
+      //   crossAxisSpacing: 10,
+      //   crossAxisCount: 2,
+      //   children: [
+      //     Container(
+      //       color: Colors.pink,
+      //     ),
+      //     Container(
+      //       color: Colors.red,
+      //     ),
+      //     Container(
+      //       color: Colors.green,
+      //     ),
+      //     Container(
+      //       color: Colors.orange,
+      //     ),
+      //     Container(
+      //       color: Colors.purple,
+      //     ),
+      //     Container(
+      //       color: Colors.yellow,
+      //     ),
+      //     Container(
+      //       color: Colors.cyan,
+      //     ),
+      //   ],
+      // ),
     );
   }
 
+  Widget _buildGridViewItem(int index) {
+    return Container(
+      color: colors[index],
+    );
+  }
+
+  static const List colors = [
+    Colors.red,
+    Colors.green,
+    Colors.orange,
+    Colors.pink,
+    Colors.blue,
+  ];
 }
